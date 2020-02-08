455f3265dde2a903ae9d40e4ebc62f915d9ecbe7
atleastzero <megan.obryan52@gmail.com>
2020-02-08 11:47:38 -0800

    Implemented LinkedListQueue

--- Code/queue.py
+++ Code/queue.py
@@ -21,28 +21,36 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        if self.list.head is None:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.size
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list.head.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
-
+        if self.is_empty():
+            raise ValueError
+        else:
+            front = self.list.head.data
+            self.list.delete(front)
+            return front
 
 # Implement ArrayQueue below, then change the assignment at the bottom
 # to use this Queue implementation to verify it passes all tests
