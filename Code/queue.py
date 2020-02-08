6b3a40772c21d6180b8ecc32d9cb784cfd91e533
atleastzero <megan.obryan52@gmail.com>
2020-02-08 11:52:57 -0800

    Implemented ArrayQueue

--- Code/queue.py
+++ Code/queue.py
@@ -60,6 +60,7 @@ class ArrayQueue(object):
         """Initialize this queue and enqueue the given items, if any."""
         # Initialize a new list (dynamic array) to store the items
         self.list = list()
+        self.start = -1
         if iterable is not None:
             for item in iterable:
                 self.enqueue(item)
@@ -70,30 +71,40 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        if self.start == -1:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.start + 1
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        self.list.insert(0, item)
+        self.start += 1
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list[self.start]
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
-
+        if self.is_empty():
+            raise ValueError
+        else:
+            front = self.list[self.start]
+            self.list[self.start] = None
+            self.start -= 1
+            return front
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
-# Queue = ArrayQueue
+# Queue = LinkedQueue
+Queue = ArrayQueue
